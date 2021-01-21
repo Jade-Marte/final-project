6baff7ee98d81011c5dd2294a80ecdf834156d77
Jordane-Allen <jordane.allen7@gmail.com>
2021-01-21 12:15:28 -0500

    First Commit

---
+++ src/NavBar.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+// import Switch from '@material-ui/core/Switch';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/link';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+
+	appBar: {
+		background: '#2ECC71',
+		color: 'white',
+		boxShadow: '0px 0px 0px 0px',
+		fontWeight: '600px',
+	},
+}));
+
+export default function MenuAppBar() {
+	const classes = useStyles();
+	const [auth, setAuth] = React.useState(true);
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+
+	const handleChange = (event) => {
+		setAuth(event.target.checked);
+	};
+
+	const handleMenu = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<div className={classes.root}>
+			<FormGroup></FormGroup>
+			<AppBar className={classes.appBar} position="sticky">
+				<Toolbar>
+					{/* <IconButton
+						edge="start"
+						className={classes.menuButton}
+						color="inherit"
+						aria-label="menu"
+					>
+						<MenuIcon />
+					</IconButton> */}
+					<Typography variant="h5" className={classes.title}>
+						Recipe App
+					</Typography>
+
+					<Button onClick={<Link to="/TestHome"></Link>} color="inherit">
+						Saved Recipes
+					</Button>
+					<Button color="inherit">Survey page</Button>
+					{auth && (
+						<div>
+							<IconButton
+								aria-label="account of current user"
+								aria-controls="menu-appbar"
+								aria-haspopup="true"
+								onClick={handleMenu}
+								color="inherit"
+							>
+								<AccountCircle />
+							</IconButton>
+							<Menu
+								id="menu-appbar"
+								anchorEl={anchorEl}
+								anchorOrigin={{
+									vertical: 'top',
+									horizontal: 'right',
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: 'top',
+									horizontal: 'right',
+								}}
+								open={open}
+								onClose={handleClose}
+							>
+								<MenuItem onClick={handleClose}>Log Out</MenuItem>
+							</Menu>
+						</div>
+					)}
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+}
