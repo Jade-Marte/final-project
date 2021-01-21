e5ef8a4710b5f2b26986f3c6540e63683a050da0
Athora190 <jademarte7@gmail.com>
2021-01-21 11:42:41 -0500

    Survey page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import RecipeResults from './pages/recipe-results.js';
+import logo from "./logo.svg";
+import "./App.css";
+import RecipeResults from "./pages/recipe-results.js";
+import Survey from "./pages/surveyPage.js";
 function App() {
   //these are test items. Delete when the program is working.
   let testResults = [
     {
-        name: "Result 1",
-        description: "One",
-        picture:"https://valentinascorner.com/wp-content/uploads/2020/07/Shrimp-Ceviche-Recipe-3.jpg"
+      name: "Result 1",
+      description: "One",
+      picture:
+        "https://valentinascorner.com/wp-content/uploads/2020/07/Shrimp-Ceviche-Recipe-3.jpg",
     },
     {
-        name: "Result 2",
-        description: "Two",
-        picture: "https://therecipecritic.com/wp-content/uploads/2015/10/porcupinemeatballs-650x975.jpg"
-
+      name: "Result 2",
+      description: "Two",
+      picture:
+        "https://therecipecritic.com/wp-content/uploads/2015/10/porcupinemeatballs-650x975.jpg",
     },
-    {   name : "Result 3",
-        description: "Three",
-        picture: "https://sweetandsavorymeals.com/wp-content/uploads/2019/05/Grilled-Eggplant-Recipe-4.jpg"
-    }
-]
+    {
+      name: "Result 3",
+      description: "Three",
+      picture:
+        "https://sweetandsavorymeals.com/wp-content/uploads/2019/05/Grilled-Eggplant-Recipe-4.jpg",
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -35,8 +39,9 @@ function App() {
         >
           Learn React
         </a> 
-      </header>
-      <RecipeResults results = { testResults}></RecipeResults>
+      </header> */}
+      <RecipeResults results={testResults}></RecipeResults>
+      <Survey />
     </div>
   );
 }
---
+++ src/pages/surveyPage.js
@@ -0,0 +1,201 @@
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import {
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  ButtonBase,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+// class userList extends component {}
+//
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+  selecteditem: {
+    border: "2px solid aqua",
+  },
+  selecteddiet: {
+    border: "2px solid aqua",
+  },
+});
+
+export default function Survey() {
+  const [items, setItems] = useState([
+    { img: "/food-pics/bananas-thumb-l.png", title: "Banana", selected: false },
+    { img: "/food-pics/soy_info.jpg", title: "Soy", selected: false },
+    {
+      img: "/food-pics/testkitchen_argentinesteak.jpg",
+      title: "Steak",
+      selected: false,
+    },
+    {
+      img: "/food-pics/Boiled-Corn-On-The-Cob-Recipe-720x405.jpg",
+      title: "Corn",
+      selected: false,
+    },
+    {
+      img: "/food-pics/shelley-pauls-t4X660oKiYs-unsplash_mmpaih.jpg",
+      title: "Beans",
+      selected: false,
+    },
+    { img: "/food-pics/a-basket-of-eggs.jpg", title: "Eggs", selected: false },
+    {
+      img: "/food-pics/yam-sweet-potato-image-block.png",
+      title: "Yam",
+      selected: false,
+    },
+    { img: "/food-pics/moreecoliill.jpg", title: "Lettuce", selected: false },
+    {
+      img:
+        "/food-pics/depositphotos_95914776-stock-photo-instant-soup-noddles-fast-food.jpg",
+      title: "Noodles",
+      selected: false,
+    },
+    {
+      img: "/food-pics/tomatoes-1296x728-feature.jpg",
+      title: "Tomatoes",
+      selected: false,
+    },
+    {
+      img: "/food-pics/tea-cup-bag-high-res-stock-photography-1570544677.jpg",
+      title: "Tea",
+      selected: false,
+    },
+    {
+      img: "/food-pics/the-health-benefits-of-tofu-700-350-1550d79.jpg",
+      title: "Tofu",
+      selected: false,
+    },
+  ]);
+  const [diets, setDiet] = useState([
+    {
+      img: "/food-pics/PF641SALADX1132-wc.jpg",
+      title: "Vegetarian",
+      selected: false,
+    },
+    {
+      img: "/food-pics/25471.jpg",
+      title: "Vegan",
+      selected: false,
+    },
+    {
+      img: "/food-pics/642x361_13_Dairy_Free_Dinner_Recipes-porkbunchan.jpg",
+      title: "Non-Dairy",
+      selected: false,
+    },
+    {
+      img: "/food-pics/Chicken-Sweet-Potato-Meal-Prep-Bowls-Recipe.jpg",
+      title: "Meat Lover",
+      selected: false,
+    },
+  ]);
+  const [selectedItems, setSelectedItmes] = useState([]);
+  function selectFood(title) {
+    setItems(
+      items.map((item) => {
+        if (item.title === title) {
+          return { ...item, selected: !item.selected };
+        } else {
+          return item;
+        }
+      })
+    );
+  }
+  function selectDiet(title) {
+    setDiet(
+      diets.map((diet) => {
+        if (diet.title === title) {
+          return { ...diet, selected: !diet.selected };
+        } else {
+          return diet;
+        }
+      })
+    );
+  }
+  let listStyle = {
+    display: "flex",
+    justifyContent: "center",
+  };
+  let style = {
+    display: "flex",
+    justifyContent: "center",
+  };
+  const classes = useStyles();
+  return (
+    <div>
+      <div style={style}>
+        <Typography variant="h3">What is you favorite food?</Typography>
+        <br></br>
+        <Typography variant="h5">choose six out of tweleve</Typography>
+      </div>
+      <div style={listStyle}>
+        {items.map((item) => {
+          return (
+            <Card
+              className={`${classes.root} ${
+                item.selected ? classes.selecteditem : ""
+              }`}
+              key={item.title}
+            >
+              <CardActionArea
+                onClick={() => {
+                  selectFood(item.title);
+                }}
+              >
+                <CardMedia
+                  className={classes.media}
+                  image={item.img}
+                  title={item.title}
+                ></CardMedia>
+                <CardContent>
+                  <Typography gutterbuttom variant="h5" component="h3">
+                    {item.title}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          );
+        })}
+      </div>
+      <div style={listStyle}>
+        <Typography variant="h3">What is your diet?</Typography>
+      </div>
+      <div style={style}>
+        {diets.map((diet) => {
+          return (
+            <Card
+              className={`${classes.root} ${
+                diet.selected ? classes.selecteddiet : ""
+              }`}
+              key={diet.title}
+            >
+              <CardActionArea
+                onClick={() => {
+                  selectDiet(diet.title);
+                }}
+              >
+                <CardMedia
+                  className={classes.media}
+                  image={diet.img}
+                  title={diet.title}
+                ></CardMedia>
+                <CardContent>
+                  <Typography gutterbuttom variant="h5" component="h3">
+                    {diet.title}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
