55e0968b9afc743d75ba2785ceaf117a86e2934b
JustDame <76237934+JustDame@users.noreply.github.com>
2021-01-21 22:02:41 -0500

    Modified

--- src/pages/recipe-results.js
+++ src/pages/recipe-results.js
@@ -1,62 +1,61 @@
-import React from 'react';
-import Carousel from 'react-material-ui-carousel'
-import { Paper, Button, Grid } from '@material-ui/core'
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button, Grid } from "@material-ui/core";
 
 class RecipeResults extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-
-    render() {
-        return (
-
-            <>
-                <Grid container>
-                    <Grid item xs={4.1}>
-                    <Carousel>
-                            {
-                                this.props.results.map((item, i) => <Item key={i} item={item} />)
-                            }
-                        </Carousel>
-                        
-	                </Grid>
-                    <Grid item xs={4.1}>
-                        <Carousel>
-                            {
-                                this.props.results.map((item, i) => <Item key={i} item={item} />)
-                            }
-                        </Carousel>
-                        <Button>
-                            View Now
-				        </Button>
-                    </Grid>
-                    <Grid item xs={4.1}>
-                    <Carousel>
-                            {
-                                this.props.results.map((item, i) => <Item key={i} item={item} />)
-                            }
-                        </Carousel>  
-	                </Grid>
-                </Grid>
-
-
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Grid container>
+          <Grid item xs={4.1}>
+            <Carousel>
+              {this.props.results.map((item, i) => (
+                <Item key={i} item={item} />
+              ))}
+            </Carousel>
+          </Grid>
+          <Grid item xs={4.1}>
+            <Carousel>
+              {this.props.results.map((item, i) => (
+                <Item key={i} item={item} />
+              ))}
+            </Carousel>
+          </Grid>
+          <Grid item xs={4.1}>
+            <Carousel>
+              {this.props.results.map((item, i) => (
+                <Item key={i} item={item} />
+              ))}
+            </Carousel>
+          </Grid>
+        </Grid>
+      </>
+    );
+  }
 }
 function Item(props) {
-    return (
-        <Paper>
-            <h2>{props.item.name}</h2>
-            <img src={props.item.picture} alt={props.item.name} style={{ width: '500px', height: '400px' }}></img>
-            <p>{props.item.description}</p>
+  const styles = {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+  };
+  return (
+    <Paper style={styles}>
+      <h2>{props.item.name}</h2>
+      <img
+        src={props.item.picture}
+        alt={props.item.name}
+        style={{ width: "500px", height: "400px" }}
+      ></img>
+      <p>{props.item.description}</p>
 
-
-            <Button className="CheckButton">
-                Check it out!
-            </Button>
-        </Paper>
-    )
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
+  );
 }
 export default RecipeResults;
