ce6bc484d3b97b3f0426f5f7a23a5acab6df2a36
Athora190 <jademarte7@gmail.com>
2021-01-22 10:14:50 -0500

    not sure why

--- src/App.js
+++ src/App.js
@@ -1,52 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-import RecipeResults from './pages/recipe-results.js';
-import NavBar from './NavBar.js';
-import React from 'react';
-import Survey from './pages/surveyPage.js';
+import logo from "./logo.svg";
+import "./App.css";
+import RecipeResults from "./pages/recipe-results.js";
+import NavBar from "./NavBar.js";
+import React from "react";
+import Survey from "./pages/surveyPage.js";
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	Link,
-	useParams,
-} from 'react-router-dom';
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+import SavedRecipes from "./pages/recipeStorage.js";
 function App() {
-	//these are test items. Delete when the program is working.
-	let testResults = [
-		{
-			name: 'Result 1',
-			description: 'One',
-			picture:
-				'https://valentinascorner.com/wp-content/uploads/2020/07/Shrimp-Ceviche-Recipe-3.jpg',
-		},
-		{
-			name: 'Result 2',
-			description: 'Two',
-			picture:
-				'https://therecipecritic.com/wp-content/uploads/2015/10/porcupinemeatballs-650x975.jpg',
-		},
-		{
-			name: 'Result 3',
-			description: 'Three',
-			picture:
-				'https://sweetandsavorymeals.com/wp-content/uploads/2019/05/Grilled-Eggplant-Recipe-4.jpg',
-		},
-	];
-	return (
-		<Router>
-			<NavBar></NavBar>
-			<div className="App">
-				<Switch>
-					<Route path="/surveyPage">
-						<Survey />
-					</Route>
-
-					<RecipeResults results={testResults}></RecipeResults>
-				</Switch>
-			</div>
-		</Router>
-	);
+  //these are test items. Delete when the program is working.
+  let testResults = [
+    {
+      name: "Result 1",
+      description: "One",
+      picture:
+        "https://valentinascorner.com/wp-content/uploads/2020/07/Shrimp-Ceviche-Recipe-3.jpg",
+    },
+    {
+      name: "Result 2",
+      description: "Two",
+      picture:
+        "https://therecipecritic.com/wp-content/uploads/2015/10/porcupinemeatballs-650x975.jpg",
+    },
+    {
+      name: "Result 3",
+      description: "Three",
+      picture:
+        "https://sweetandsavorymeals.com/wp-content/uploads/2019/05/Grilled-Eggplant-Recipe-4.jpg",
+    },
+  ];
+  return (
+    <div>
+      <Router>
+        <NavBar></NavBar>
+        <div className="App">
+          <Switch>
+            <Route path="/surveyPage">
+              <Survey />
+            </Route>
+            <Route path="/savedPage">
+              <SavedRecipes />
+            </Route>
+            <RecipeResults results={testResults}></RecipeResults>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
