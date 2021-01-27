c1c7a0bf16772f64b315a2ebe47a9820ec29d021
Jordane-Allen <jordane.allen7@gmail.com>
2021-01-27 01:36:58 -0500

    Added Carousel Landing Page and its corresponding data

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,8 +8,8 @@ import {
 	Grid,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import transitions from '@material-ui/core/styles/transitions';
 import { Link } from 'react-router-dom';
+import Carousel from 'react-material-ui-carousel';
 
 const useStyles = makeStyles({
 	container: {
@@ -25,8 +25,12 @@ const useStyles = makeStyles({
 		color: 'white',
 	},
 	title: {
-		fontWeight: '500',
-		color: 'rgba(62,204,63,0.9)',
+		fontWeight: '600',
+		background:
+			'-webkit-linear-gradient(90deg,  rgba(42,204,33,1) 55%, rgba(27,111,62,1) 80%, rgba(55,245,77,0.989233193277311) 10%);',
+
+		'-webkit-background-clip': 'text',
+		'-webkit-text-fill-color': 'transparent',
 	},
 	overlay: {
 		backgroundColor: 'rgba(20, 20, 20, 0.7)',
@@ -45,17 +49,35 @@ const useStyles = makeStyles({
 		background:
 			'linear-gradient(90deg, rgba(42,204,33,0.9220063025210083) 0%, rgba(46,204,65,1) 35%, rgba(35,157,86,1) 100%)',
 		color: 'white',
-		opacity: 0.8,
+		opacity: 1,
 		'&:hover': {
-			opacity: 1,
+			opacity: 0.8,
 		},
 		transition: 'opacity 200ms',
 
 		textDecoration: 'none',
 	},
 
+	signUpButton: {
+		marginRight: '20px',
+		fontWeight: '300',
+		color: 'rgba(42,204,33,0.9220063025210083)',
+		opacity: 1,
+		border: '1px solid rgba(42,204,33,0.9220063025210083)',
+		'&:hover': {
+			background:
+				'linear-gradient(90deg, rgba(42,204,33,0.9220063025210083) 0%, rgba(46,204,65,1) 35%, rgba(35,157,86,1) 100%)',
+			color: 'white',
+			border: '0px',
+		},
+		transition: 'opacity 200ms',
+
+		textDecoration: 'none',
+	},
 	subtitle: {
-		fontSize: 30,
+		fontSize: 40,
+		fontFamily: 'Kalam, cursive',
+		fontWeight: '450',
 		marginLeft: 'auto',
 		marginRight: 'auto',
 		marginBottom: '30px',
@@ -63,11 +85,47 @@ const useStyles = makeStyles({
 });
 
 export default function LandingPage(props) {
+	const items = [
+		{
+			image: '/food-pics/eat.jpg',
+			description: 'WE SERVE UP GREAT RECIPES FOR ALL DIETS',
+		},
+		{
+			image: '/food-pics/asparagus.jpg',
+			description: 'FEED YOUR MEAT CRAVINGS',
+		},
+		{
+			image: '/food-pics/piza.jpg',
+			description: 'SCRUMPTIOUS DELIGHTS',
+		},
+		{
+			image: '/food-pics/salmon.jpg',
+			description: 'READY TO GO',
+		},
+		{
+			image: '/food-pics/meal.jpg',
+			description: 'HEALTHY OPTIONS',
+		},
+		{
+			image: '/food-pics/smoothies.jpg',
+			description: 'FRUITY DELIGHTS',
+		},
+	];
+
+	return (
+		<Carousel animation="slide" timeout="200">
+			{items.map((item, i) => (
+				<Item key={i} item={item} />
+			))}
+		</Carousel>
+	);
+}
+function Item(props) {
 	const classes = useStyles();
 	return (
 		<Box
 			style={{
-				backgroundImage: `url(${props.image || '/food-pics/eat.jpg'})`,
+				backgroundImage: `url(${props.item.image})`,
 				backgroundPosition: '100% center',
 			}}
 			className={classes.container}
@@ -83,15 +141,26 @@ export default function LandingPage(props) {
 							{props.title || 'Recipe App'}
 						</Typography>
 					</Fade>
-					<Fade in={true} timeout={1500}>
+					<Fade in={true} timeout={200}>
 						<Typography
 							variant="body1"
 							gutterBottom
 							className={`${classes.text} ${classes.subtitle}`}
 						>
-							{props.description || 'We serve up great recipes for diets.'}
+							{props.item.description}
 						</Typography>
 					</Fade>
+					<Link style={{ textDecoration: 'none' }} to="/register">
+						{' '}
+						<Button
+							className={`${classes.signUpButton} ${classes.hover}`}
+							type="Submit"
+							variant="outlined"
+							size="large"
+						>
+							Sign Up
+						</Button>
+					</Link>
 					<Link style={{ textDecoration: 'none' }} to="/login">
 						{' '}
 						<Button
@@ -103,17 +172,6 @@ export default function LandingPage(props) {
 							Login{' '}
 						</Button>
 					</Link>{' '}
-					<Link style={{ textDecoration: 'none' }} to="/register">
-						{' '}
-						<Button
-							className={`${classes.button} ${classes.hover}`}
-							type="Submit"
-							variant="contained"
-							size="large"
-						>
-							Sign Up
-						</Button>
-					</Link>
 				</Container>
 			</Box>
 		</Box>
