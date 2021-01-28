2d3b506c5d2deeaec1bec8340399f80065824fed
Athora190 <jademarte7@gmail.com>
2021-01-28 00:10:43 -0500

    finishing touches bugs still need fixing

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -1,30 +1,30 @@
-import React, { useContext, createContext, useState } from 'react'
-import { Redirect, Route } from 'react-router-dom'
+import React, { useContext, createContext, useState } from "react";
+import { Redirect, Route } from "react-router-dom";
 
 const auth = {
   isAuthenticated: false,
   user: {},
   async signin(username, password) {
-    console.log(process.env)
+    console.log(process.env);
     return fetch(`${process.env.REACT_APP_NODE_URL}/login`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         username,
         password,
       }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      credentials: 'include',
+      credentials: "include",
     })
       .then((res) => Promise.all([Promise.resolve(res), res.json()]))
       .then(([res, body]) => {
         if (res.ok) {
-          auth.isAuthenticated = true
-          auth.user = body.user
+          auth.isAuthenticated = true;
+          auth.user = body.user;
         }
-        return [res, body]
-      })
+        return [res, body];
+      });
   },
   async register({
     first_name,
@@ -34,7 +34,7 @@ const auth = {
     password_confirmation,
   }) {
     return fetch(`${process.env.REACT_APP_NODE_URL}/register`, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         first_name,
         last_name,
@@ -43,62 +43,62 @@ const auth = {
         password_confirmation,
       }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      credentials: 'include',
+      credentials: "include",
     })
       .then((res) => Promise.all([Promise.resolve(res), res.json()]))
       .then(([res, body]) => {
         if (res.ok) {
-          auth.isAuthenticated = true
-          auth.user = body.user
+          auth.isAuthenticated = true;
+          auth.user = body.user;
         }
-        return [res, body]
-      })
+        return [res, body];
+      });
   },
   async signout() {
     // TODO: send logout request
-    auth.isAuthenticated = false
-    auth.user = null
+    auth.isAuthenticated = false;
+    auth.user = null;
   },
-}
+};
 
 /** For more details on
  * `authContext`, `ProvideAuth`, `useAuth` and `useProvideAuth`
  * refer to: https://usehooks.com/useAuth/
  */
-export const authContext = createContext()
+export const authContext = createContext();
 
 export function ProvideAuth({ children }) {
-  const auth = useProvideAuth()
-  return <authContext.Provider value={auth}>{children}</authContext.Provider>
+  const auth = useProvideAuth();
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
 
 export function useAuth() {
-  return useContext(authContext)
+  return useContext(authContext);
 }
 
 export function useProvideAuth() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   const signin = (username, password) => {
     return auth.signin(username, password).then(([res, body]) => {
-      setUser(body.user)
-      return [res, body]
-    })
-  }
+      setUser(body.user);
+      return [res, body];
+    });
+  };
 
   const signout = (cb) => {
     return auth.signout().then(() => {
-      setUser(null)
-    })
-  }
+      setUser(null);
+    });
+  };
 
   return {
     user,
     signin,
     signout,
-  }
+  };
 }
 
 export function PrivateRoute({ component: Component, ...rest }) {
@@ -111,12 +111,12 @@ export function PrivateRoute({ component: Component, ...rest }) {
         ) : (
           <Redirect
             to={{
-              pathname: '/login',
+              pathname: "/login",
               state: { from: props.location },
             }}
           />
         )
       }
     />
-  )
+  );
 }
--- src/pages/recipe-results.js
+++ src/pages/recipe-results.js
@@ -3,7 +3,7 @@ import Carousel from "react-material-ui-carousel";
 import { Paper, Button, Grid } from "@material-ui/core";
 import RecipeContext from "../components/RecipesContext";
 import axios from "axios";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 class RecipeResults extends React.Component {
   constructor(props) {
@@ -26,27 +26,27 @@ class RecipeResults extends React.Component {
     return (
       <RecipeContext.Consumer>
         {({ recipes, setRecipes }) => (
-        <Grid container>
-          {/*} <Grid item xs={4.1}>
+          <Grid container>
+            {/*} <Grid item xs={4.1}>
             <img
               src="https://tse2.mm.bing.net/th?id=OIP.xaF966Gpx9WWO1X81pirRwHaE8&pid=Api&P=0&w=231&h=155"
               style={{ width: "500px", height: "400px",paddingTop: "72px" }}
             ></img>
                </Grid>*/}
-          <Grid item xs={12}>
-            <Carousel index="1">
-              {recipes.map((item, i) => (
-                <Item key={i} item={item} />
-              ))}
-            </Carousel>
-          </Grid>
-          {/*}  <Grid item xs={4.1}>
+            <Grid item xs={12}>
+              <Carousel index="1">
+                {recipes.map((item, i) => (
+                  <Item key={i} item={item} />
+                ))}
+              </Carousel>
+            </Grid>
+            {/*}  <Grid item xs={4.1}>
             <img
               src="https://tse1.mm.bing.net/th?id=OIP.KDFklMdaYEXT32cep3qqjgHaFR&pid=Api&P=0&w=216&h=155"
               style={{ width: "500px", height: "400px",paddingTop: "72px" }}
             ></img>
               </Grid>*/}
-        </Grid>
+          </Grid>
         )}
       </RecipeContext.Consumer>
     );
@@ -59,6 +59,17 @@ function Item(props) {
     flexDirection: "column",
     alignItems: "center",
   };
+  function Savedrecipe(id) {
+    axios
+      .post(
+        process.env.REACT_APP_NODE_URL + "/saved-recipe",
+        { recipeId: id },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
   return (
     <Paper style={styles}>
       <h2>{props.item.title}</h2>
@@ -68,8 +79,17 @@ function Item(props) {
         style={{ height: "400px", width: "Auto" }}
       ></img>
       <p>{props.item.summary}</p>
-
-      <Button className="CheckButton"> <Link to={`/view-recipe/${props.item.id}`}>Check it out!</Link></Button>
+      <Button
+        onClick={() => {
+          Savedrecipe(props.item.id);
+        }}
+      >
+        save recipe
+      </Button>
+      <Button className="CheckButton">
+        {" "}
+        <Link to={`/view-recipe/${props.item.id}`}>Check it out!</Link>
+      </Button>
     </Paper>
   );
 }
